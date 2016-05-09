8564a8a29bce4303b858c939da0e465515e23f13
Bhavya <Bhavya@Rasheed>
2016-05-09 12:54:25 +0530

    changed for testing helloword.jsp

---
+++ src/sampleshelloworld/Samplehelloworld.java
@@ -0,0 +1,10 @@
+package sampleshelloworld;
+
+public class Samplehelloworld {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("sample program");
+	}
+
+}
